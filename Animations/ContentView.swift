cd3db3c11f3e7ac299d6fd9ea507a7b76778d93a
Grattours <grattours@gmail.com>
2019-10-26 15:11:46 +0200

    J33 fin

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,80 +8,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var animationAmount = 0.0
-    var body: some View {
-        Button("Tappez Moi !") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
-            }
-//            withAnimation {
-//                self.animationAmount += 360
-//            }
-        }
-        .padding(50)
-        .background(Color.blue)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
     }
 }
 
-//struct ContentView: View {
-//    @State private var animationAmount: CGFloat = 1
-//
-//    var body: some View {
-//        print(animationAmount)
-//
-//        return VStack {
-////            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
-//            Stepper("Scale amount", value: $animationAmount.animation(
-//                Animation.easeInOut(duration: 1)
-//                    .repeatCount(3, autoreverses: true)
-//            ), in: 1...10)
-//
-//            Spacer()
-//
-//            Button("Tap Me") {
-//                self.animationAmount += 1
-//            }
-//            .padding(40)
-//            .background(Color.red)
-//            .foregroundColor(.white)
-//            .clipShape(Circle())
-//            .scaleEffect(animationAmount)
-//        }
-//    }
-//}
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
 
-//struct ContentView: View {
-//
-//    @State private var animationAmount: CGFloat = 1
-//
-//    var body: some View {
-//        Button("Tap Me") {
-//            self.animationAmount += 1
-//        }
-//        .padding(40)
-//        .background(Color.red)
-//        .foregroundColor(.white)
-//        .clipShape(Circle())
-//        .overlay(
-//            Circle()
-//                .stroke(Color.red)
-//                .scaleEffect(animationAmount)
-//                .opacity(Double(2 - animationAmount))
-//                .animation(
-//                    Animation.easeOut(duration: 1)
-//                        .repeatForever(autoreverses: false)
-//            )
-//        )
-//            .onAppear {
-//                self.animationAmount = 2
-//        }
-//
-//    }
-//}
+struct ContentView: View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tappez Moi !") {
+                self.isShowingRed.toggle()
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+                
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
